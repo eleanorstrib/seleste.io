642f23c01e215b9a76dce644a76950e697101b9c
eleanorstrib <eleanor@sloan.mit.edu>
2016-03-06 10:30:23 -0800

    added some styling and changes to js file for animations

--- search/static/scripts/validationscript.js
+++ search/static/scripts/validationscript.js
@@ -2,48 +2,48 @@ $(document).ready(function(){
 	// change the size of the image based on the value selected in the box
 	$('input#culturevalues').change(function(){
 		$('img#culturevalues').animate({
-    	left: "+=50",
-    	width: parseInt($('input#culturevalues').val()) + 20,
+    	left: "+=80",
+    	width: parseInt(($('input#culturevalues').val()))*2*2 + 35,
   		}, 2000, function() {
   	});
 	});
 
 	$('input#opportunities').change(function(){
 		$('img#opportunities').animate({
-    	left: "+=50",
-    	width: parseInt($('input#opportunities').val()) + 20,
+    	left: "+=80",
+    	width: parseInt(($('input#opportunities').val()))*2 + 35,
   		}, 2000, function() {
   	});
 	});
 
 	$('input#management').change(function(){
 		$('img#management').animate({
-    	left: "+=50",
-    	width: parseInt($('input#management').val()) + 20,
+    	left: "+=80",
+    	width: parseInt(($('input#management').val()))*2 + 35,
   		}, 2000, function() {
   	});
 	});
 
 	$('input#compensationbenefits').change(function(){
 		$('img#compensationbenefits').animate({
-    	left: "+=50",
-    	width: parseInt($('input#compensationbenefits').val()) + 20,
+    	left: "+=80",
+    	width: parseInt(($('input#compensationbenefits').val()))*2 + 35,
   		}, 2000, function() {
   	});
 	});
 
 	$('input#worklifebalance').change(function(){
 		$('img#worklifebalance').animate({
-    	left: "+=50",
-    	width: parseInt($('input#worklifebalance').val()) + 20,
+    	left: "+=80",
+    	width: parseInt(($('input#worklifebalance').val()))*2 + 35,
   		}, 2000, function() {
   	});
 	});
 
 	$('input#recommendtoafriend').change(function(){
 		$('img#recommendtoafriend').animate({
-    	left: "+=50",
-    	width: parseInt($('input#recommendtoafriend').val()) + 20,
+    	left: "+=80",
+    	width: parseInt(($('input#recommendtoafriend').val()))*2 + 35,
   		}, 2000, function() {
   	});
 	});
