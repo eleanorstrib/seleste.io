fc2f753afacb47e87a249733865cce86e160206b
eleanorstrib <eleanor@sloan.mit.edu>
2016-03-08 12:29:59 -0800

    added an object created on submit with user preferences, will pass to backend

--- search/static/scripts/validationscript.js
+++ search/static/scripts/validationscript.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
-
+	var priorities = {}
 	// change the size of the image based on the value selected in the box
 	$('input#culturevalues').change(function(){
 		$('img#culturevalues').animate({
     	left: "+=80",
     	width: parseInt(($('input#culturevalues').val()))*2 + 35,
   		}, 2000, function() {
-  			updateTotal()
+  			checkTotal()
   	});
 	});
 
@@ -15,7 +15,7 @@ $(document).ready(function(){
     	left: "+=80",
     	width: parseInt(($('input#opportunities').val()))*2 + 35,
   		}, 2000, function() {
-  			updateTotal()
+  			checkTotal()
   	});
 	});
 
@@ -24,7 +24,7 @@ $(document).ready(function(){
     	left: "+=80",
     	width: parseInt(($('input#management').val()))*2 + 35,
   		}, 2000, function() {
-  			updateTotal()
+  			checkTotal()
   	});
 	});
 
@@ -33,7 +33,7 @@ $(document).ready(function(){
     	left: "+=80",
     	width: parseInt(($('input#compensationbenefits').val()))*2 + 35,
   		}, 2000, function() {
-  			updateTotal()
+  			checkTotal()
   	});
 	});
 
@@ -42,48 +42,40 @@ $(document).ready(function(){
     	left: "+=80",
     	width: parseInt(($('input#worklifebalance').val()))*2 + 35,
   		}, 2000, function() {
-  			updateTotal()
-  	});
-	});
-
-	$('input#recommendtoafriend').change(function(){
-		$('img#recommendtoafriend').animate({
-    	left: "+=80",
-    	width: parseInt(($('input#recommendtoafriend').val()))*2 + 35,
-  		}, 2000, function() {
-  			updateTotal()
+  			checkTotal()
   	});
 	});
 
 	function sumTotal(){
 		var currentTotal = parseInt(($('input#culturevalues').val())) + parseInt(($('input#opportunities').val())) +  
 		parseInt(($('input#management').val())) + parseInt(($('input#compensationbenefits').val())) +  
-		parseInt(($('input#worklifebalance').val())) + parseInt(($('input#recommendtoafriend').val()));
-		$('div.error-msg').html("You've allocated " + currentTotal + "of your preferences.  You have " + (100-currentTotal) + "% left.");
+		parseInt(($('input#worklifebalance').val()));
 		return currentTotal;
 	}
 
 	function checkTotal(){
-		if (sumTotal() === 100){
+		var currentTotal = sumTotal()
+		if (currentTotal < 100) {
+			$('div.total-msg').html("You've allocated " + currentTotal + " points. " + (100-currentTotal) + " left!");
+			return false;
+		} else if (currentTotal === 100) {
+			$('div.total-msg').html("Nice work!  You've allocated all 100 points.");
 			return true;
 		} else {
+			$('div.total-msg').html("Oops, you've over allocated by " + (currentTotal-100) + " points.  Try rebalancing.");
 			return false;
 		}
 	};
 
-	function updateTotal(){
-		if (checkTotal()){
-			console.log("looks good");
-		} else {
-			console.log("not quite");
-		}
-	}
-
 	$("#priorities-form").on('submit', function(e) {
 		e.preventDefault();
 		if (checkTotal()) {
-			console.log("OK");
-			return ("OK");
+			priorities.culturevalues = parseInt(($('input#culturevalues').val()));
+			priorities.opportunities = parseInt(($('input#opportunities').val()));
+			priorities.management = parseInt(($('input#management').val()));
+			priorities.compensationbenefits = parseInt(($('input#compensationbenefits').val()));
+			priorities.worklifebalance = parseInt(($('input#worklifebalance').val()));
+			console.log(priorities);
 		} else {
 			console.log("Not ok");
 			return ("Not ok");
--- search/views.py
+++ search/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
 
 def home(request):
+	print(request.session)
+	if request.method == 'POST':
+		return HTTPResponseRedirect('/companies/')
 	return render(request, 'search/index.html')
+
+def companies(request):
+
+	return render(request, 'search/companies.html')
+
+def results(request):
+	return render(request, 'search/results.html')
