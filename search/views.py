040b8eb56fc66114bbe9c684249fdd47210ef42d
eleanorstrib <eleanor@sloan.mit.edu>
2016-03-21 15:43:06 -0700

    having trouble decoding the bytes format to json, but otherwise working, will return to that

--- search/views.py
+++ search/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, render_to_response
 from django.http import HttpResponseRedirect, HttpResponse
-from django.views.decorators.csrf import ensure_csrf_cookie 
+from django.views.decorators.csrf import ensure_csrf_cookie
+from search.models import Company, Glassdoor, Indeed 
 import json
 
 def home(request):
@@ -13,6 +14,20 @@ def companies(request):
 
 
 def results(request):
-	data= request.body
-	print(data)
+	data = request.body.decode()
+	data = json.loads(data)
+	print(type(data))
+	# for record in gd_data:
+	# 	print(gd_data['name'])
+	# 	models.Company.company_name = gd_data['name']
+	# 	models.Glassdoor.company_name = gd_data['name']
+	# 	models.Glassdoor.overall_rating = gd_data['overallRating']
+	# 	models.Glassdoor.number_ratings = gd_data['numberOfRatings']
+	# 	models.Glassdoor.culture_values_rating = gd_data['cultureAndValuesRating']
+	# 	models.Glassdoor.management_rating = gd_data['seniorLeadershipRating']
+	# 	models.Glassdoor.compensation_benefits_rating = gd_data['compensationAndBenefitsRating']
+	# 	models.Glassdoor.opportunities_rating = gd_data['careerOpportunitiesRating']
+	# 	models.Glassdoor.work_life_balance_rating = gd_data['workLifeBalanceRating']
+	# 	models.Glassdoor.recommend_to_friend_rating = gd_data['recommendToFriendRating']
+
 	return render(request, 'search/results.html', {})
