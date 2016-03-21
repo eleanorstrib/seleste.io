9608ddb847f544d656139ff25e4ffcbd66e3d00b
eleanorstrib <eleanor@sloan.mit.edu>
2016-03-21 14:23:30 -0700

    fixed the issues with CSRF tokens, data is now being passed to the backend

--- search/views.py
+++ search/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render, render_to_response
-from django.http import HttpResponseRedirect, HttpResponse 
+from django.http import HttpResponseRedirect, HttpResponse
+from django.views.decorators.csrf import ensure_csrf_cookie 
 import json
 
 def home(request):
 	return render(request, 'search/index.html', {})
 
+@ensure_csrf_cookie
 def companies(request):
+
 	return render_to_response('search/companies.html', {})
 
 
 def results(request):
+	data= request.body
+	print(data, "data")
 	return render(request, 'search/results.html', {})
