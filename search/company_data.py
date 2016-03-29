eb4a5c87dee2b7476b905b2a098c7352f4e9e744
eleanorstrib <eleanor@sloan.mit.edu>
2016-03-28 21:02:06 -0700

    got calcs and addition to dict working

--- search/company_data.py
+++ search/company_data.py
@@ -9,27 +9,32 @@ def merge_data(all_company_data):
 	for company in all_company_data:
 		print ("this is company", company)
 		
-		# if all_company_data[company][0]['glassdoor'] and all_company_data[company][1]['indeed']:
-		gd_index = all_company_data[company][0]['glassdoor']
-		in_index = all_company_data[company][1]['indeed']
-	
-		num_gd_ratings = int(gd_index['numberOfRatings'])
-		num_in_ratings = int(in_index['TotalReviews'])
-		num_reviews = num_gd_ratings + num_in_ratings
+		if all_company_data[company][0]['glassdoor'] and all_company_data[company][1]['indeed']:
+			gd_index = all_company_data[company][0]['glassdoor']
+			in_index = all_company_data[company][1]['indeed']
+		
+			num_gd_ratings = int(gd_index['numberOfRatings'])
+			num_in_ratings = int(in_index['TotalReviews'])
+			num_reviews = num_gd_ratings + num_in_ratings
 
-		print(num_gd_ratings, num_in_ratings, num_reviews)
-		# 	pct_ratings_gd = float(num_gd_ratings/num_reviews)
-		# 	pct_ratings_in = float(num_in_ratings/num_reviews)
+			pct_ratings_gd = float(num_gd_ratings/num_reviews)
+			pct_ratings_in = float(num_in_ratings/num_reviews)
+			print(pct_ratings_in, pct_ratings_gd)
+			print (type(pct_ratings_in))
+			
+			# calculate weighted averages and add to new dict
+			summarized_data = {}
+			print('(float(gd_index[overallRating])*pct_ratings_gd)', (float(gd_index['overallRating'])*pct_ratings_gd))
+			print('(float(in_index[Overall])*pct_ratings_in)', (float(in_index['Overall'])*pct_ratings_in))
+			print((float(gd_index['overallRating'])*pct_ratings_gd) + (float(in_index['Overall'])*pct_ratings_in))
+			summarized_data['overall'] = (float(gd_index['overallRating'])*pct_ratings_gd) + (float(in_index['Overall'])*pct_ratings_in)
+			summarized_data['work_life'] = (float(gd_index['workLifeBalanceRating'])*pct_ratings_gd) + (float(in_index['Work/Life Balance'])*pct_ratings_in)
+			summarized_data['opportunities'] = (float(gd_index['careerOpportunitiesRating'])*pct_ratings_gd) + (float(in_index['Job Security/Advancement'])*pct_ratings_in)
+			summarized_data['compensation_benefits'] = (float(gd_index['compensationAndBenefitsRating'])*pct_ratings_gd) + (float(in_index['Compensation/Benefits'])*pct_ratings_in)
+			summarized_data['management'] = (float(gd_index['seniorLeadershipRating'])*pct_ratings_gd) + (float(in_index['Management'])*pct_ratings_in)
+			summarized_data['culture_values'] = (float(gd_index['cultureAndValuesRating'])*pct_ratings_gd) + (float(in_index['Culture'])*pct_ratings_in)
+			(all_company_data[company]).append(summarized_data)
+			print(all_company_data)
 
-		# 	# calculate weighted averages
-		# 	refined_company_data[company]['overall'] = (float(gd_index['overallRating'])*pct_ratings_gd) + float((in_index['Overall']*pct_ratings_in))
-		# 	refined_company_data[company]['work_life'] = (float(gd_index['workLifeBalanceRating'])*pct_ratings_gd) + float((in_index['Work/Life Balance']*pct_ratings_in))
-		# 	refined_company_data[company]['opportunities'] = (float(gd_index['careerOpportunitiesRating'])*pct_ratings_gd) + float((in_index['Job Security/Advancement']*pct_ratings_in))
-		# 	refined_company_data[company]['compensation_benefits'] = (float(gd_index['compensationAndBenefitsRating'])*pct_ratings_gd) + float((in_index['Compensation/Benefits']*pct_ratings_in))
-		# 	refined_company_data[company]['management'] = (float(gd_index['seniorLeadershipRating']*pct_ratings_gd)) + float((in_index['Management']*pct_ratings_in))
-		# 	refined_company_data[company]['culture_values ']= (float(gd_index['cultureAndValuesRating']*pct_ratings_gd)) + float((in_index['Culture']*pct_ratings_in))
-
-		# 	print('refined_company_data : ',refined_company_data)
-
-		# else: 
-		# 	print ("something went wrong with ", gd_index['name'])
+		else: 
+			print ("something went wrong with ", gd_index['name'])
