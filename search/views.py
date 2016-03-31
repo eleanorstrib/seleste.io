0a33d72752c4e6cb2f93945d1655b90d51d38e6d
eleanorstrib <eleanor@sloan.mit.edu>
2016-03-31 16:49:30 -0700

    still some odd behavior with results view

--- search/views.py
+++ search/views.py
@@ -52,7 +52,7 @@ def results(request):
 		for co in all_company_data:
 			ranked_cos[co] = all_company_data[co][2]
 		print ("this is the ranked company list: ", ranked_cos)
-		return render(request, 'search/results.html', {})
+		return render(request, 'search/results.html', {'ranked_cos': ranked_cos})
 
 	
 
